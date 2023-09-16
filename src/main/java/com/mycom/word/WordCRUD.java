00d39bd1538c162fd20024e5d52837bd22936791
= <=>
2023-09-16 17:01:21 +0900

    bug fix

--- src/main/java/com/mycom/word/WordCRUD.java
+++ src/main/java/com/mycom/word/WordCRUD.java
@@ -103,7 +103,7 @@ public class WordCRUD implements ICRUD{
         System.out.println("--------------------------------");
         for(int i = 0; i < list.size(); i++) {
             int inputLevel = list.get(i).getLevel();
-            if(inputLevel == level) continue;
+            if(inputLevel != level) continue;
             System.out.print((j+1) + " ");
             System.out.println(list.get(i).toString());
             j++;
